c83a490ff013f82b7ef7b56ae67ee05fc6b8ac23
Beto Dealmeida <roberto@dealmeida.net>
2024-11-21 18:52:15 -0500

    Fix test

--- tests/integration_tests/charts/api_tests.py
+++ tests/integration_tests/charts/api_tests.py
@@ -1964,8 +1964,11 @@ class TestChartApi(ApiOwnersTestCaseMixin, InsertChartMixin, SupersetTestCase):
         assert rv.status_code == 200
         data = json.loads(rv.data.decode("utf-8"))
 
+        data["result"].sort(key=lambda x: x["datasource_id"])
         assert data["result"][0]["slice_name"] == "name0"
         assert data["result"][0]["datasource_id"] == 1
+        assert data["result"][1]["slice_name"] == "Top 10 California Names Timeseries"
+        assert data["result"][1]["datasource_id"] == 2
 
     @parameterized.expand(
         [
